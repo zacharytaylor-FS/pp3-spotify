d682a1b256de106870161ab5a6e85c0d3b2a2d02
zacharytaylor-FS <ZJTaylor@student.fullsail.edu>
2023-09-03 23:06:38 -0400

    Update index.js and install dev dep

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-const express = require(express)
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const spotify = require('./middlewares/spotify')
+const app = express()
+
+app.use(express.urlencoded({extended: false}))
+app.use(cors())
+
+app.use('/spotify/v1', spotify)
+
+app.listen(process.env.PORT, () => console.log('Express Server is running on port:' + process.env.PORT + '! 🌎'))
